086b8295c4c8c080e7a00e9b99823c7ec4d06bac
Pluxxa <95534978+Pluxxa@users.noreply.github.com>
2024-04-15 23:46:06 +0300

    change combot sistem

--- main.py
+++ main.py
@@ -29,22 +29,44 @@ class Game():
         for _ in range(100):
             ini1 = random.randint(0, 10)
             ini2 = random.randint(0, 10)
-            if self.hero.is_alive() == True and self.computer.is_alive() == True:
-                print(f"Раунд {_+1}\n")
-                time.sleep(1)
-                self.hero.attack(other=self.computer)
-                print(f"У {self.computer.name} осталось {self.computer.hp} жизней\n")
-                time.sleep(1)
-                self.computer.attack(other=self.hero)
-                print(f"У {self.hero.name} осталось {self.hero.hp} жизней\n")
-                time.sleep(1)
+            if ini1 >= ini2:
+                if self.hero.is_alive() == True:
+                    print(f"Раунд {_+1}\n{self.hero.name} и {self.computer.name} бросают инициативу\n{self.hero.name} выбросил {ini1}\n{self.computer.name} выбросил {ini2}\n{self.hero.name} ходит первым\n")
+                    time.sleep(2)
+                    self.hero.attack(other=self.computer)
+                    print(f"У {self.computer.name} осталось {self.computer.hp} жизней\n")
+                    time.sleep(2)
+                    if self.computer.is_alive() == True:
+                        self.computer.attack(other=self.hero)
+                        print(f"У {self.hero.name} осталось {self.hero.hp} жизней\n")
+                        time.sleep(2)
+                        if self.hero.is_alive() == True:
+                            continue
+                        else:
+                            print(f"{self.computer.name} победил!!!")
+                            exit()
+                    else:
+                        print(f"{self.hero.name} победил!!!")
+                        exit()
             else:
                 if self.computer.is_alive() == True:
-                    print(f"{self.computer.name} победил!!!")
-                    exit()
-                else:
-                    print(f"{self.hero.name} победил!!!")
-                    exit()
+                    print(f"Раунд {_+1}\n{self.hero.name} и {self.computer.name} бросают инициативу\n{self.hero.name} выбросил {ini1}\n{self.computer.name} выбросил {ini2}\n{self.computer.name} ходит первым\n")
+                    time.sleep(2)
+                    self.computer.attack(other=self.hero)
+                    print(f"У {self.hero.name} осталось {self.hero.hp} жизней\n")
+                    time.sleep(2)
+                    if self.hero.is_alive() == True:
+                        self.hero.attack(other=self.computer)
+                        print(f"У {self.computer.name} осталось {self.computer.hp} жизней\n")
+                        time.sleep(2)
+                        if self.computer.is_alive() == True:
+                            continue
+                        else:
+                            print(f"{self.hero.name} победил!!!")
+                            exit()
+                    else:
+                        print(f"{self.computer.name} победил!!!")
+                        exit()
 
 
 game = Game()
